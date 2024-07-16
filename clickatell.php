ec08b4571d4abf48e8889355a2bb03629d5e1348
Jamie McClelland <jm@mayfirst.org>
2024-07-16 15:34:32 -0400

    remove functions removed in civix upgrade

--- clickatell.php
+++ clickatell.php
@@ -10,15 +10,6 @@ function clickatell_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function clickatell_civicrm_xmlMenu(&$files) {
-  _clickatell_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function clickatell_civicrm_install() {
@@ -89,19 +80,6 @@ function clickatell_civicrm_disable() {
     }
   }
   return _clickatell_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function clickatell_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _clickatell_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
